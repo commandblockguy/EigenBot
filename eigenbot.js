b075ea797e07a330df11701d75f3d4872d3c4714
commandblockguy <ecesarz98@gmail.com>
2018-01-06 10:29:35 -0600

    Added !help and !mcstatus
    Added !help and !mcstatus, which checks Mojang server status.
    Also readded that dependency.

--- eigenbot.js
+++ eigenbot.js
@@ -2,6 +2,8 @@ const config = require('./config.json');
 const regexPattern = /!(mc|mcapi|mcce|mcds|mcl|mcpe|realms|sc|web)-[0-9]{1,7}/gi;
 const urlRegex = /https:\/\/bugs.mojang.com\/browse\/(mc|mcapi|mcce|mcds|mcl|mcpe|realms|sc|web)-[0-9]{1,7}/gi;
 
+const request = require('request');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -35,6 +37,45 @@ client.on('message', msg => {
 				return;
 			} else {
 				sendEmbed(msg.channel, issue);
+			}
+		});
+	}
+	if(msg.content.startsWith(config.prefix + "help")) {
+		msg.channel.send({embed: {
+			title: "!help",
+			description: "I listen for Minecraft bug report links or " + config.prefix + "PROJECT-NUMBER\n" +
+						 "For example, saying https://bugs.mojang.com/browse/MC-81098 or !MC-81098 will give quick info on those bugs",
+			fields: [
+				{
+					name: "Other commands: ",
+					value: "**" + config.prefix + "help:** Shows this help screen.\n" +
+					       "**" + config.prefix + "mcstatus:** Checks Mojang server status. (Currently unimplemented)"
+				}
+			],
+			url: config.url,
+			color: 9441545,
+			footer: {
+				text: "EigenBot"
+			}
+		}});
+	}
+	if(msg.content.startsWith(config.prefix + "mcstatus")) {
+		request('https://status.mojang.com/check', function (error, response, body) {
+			if(error) {
+				msg.channel.send("Unable to reach Mojang API for status check. Let's assume everything went wrong.");
+				return;
+			}
+			const statuses = JSON.parse(body);
+			if(statuses.every(function(service) {
+				name = Object.keys(service)[0];
+				if(service[name] == 'green') {
+					return true;
+				} else {
+					msg.channel.send("Service " + name + " has status " + service[name]);
+					return false;
+				}
+			})) {
+				msg.channel.send("All services are working normally.");
 			}
 		});
 	}
